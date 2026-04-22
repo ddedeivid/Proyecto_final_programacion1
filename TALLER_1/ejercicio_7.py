8c800122bca2aab47a57a719a47862a5b1ac53a5
Norma Constanza Giraldogit <conynor@gmail.com>
2026-04-22 15:37:48 -0500

    Se agrega ejercicio 8

--- TALLER_1/ejercicio_7.py
+++ TALLER_1/ejercicio_7.py
@@ -1,19 +1,25 @@
 from abc import ABC, abstractmethod
+"""Se imoporta ABC y abstractmethod para crear una clase abstracta"""
 
 class Animal(ABC):
+    """Se crea la clase abstracta Animal"""
     @abstractmethod
     def hacer_sonido(self):
         pass
 class Perro(Animal):
+    """Se crea la clase Perro que hereda de Animal"""
     def hacer_sonido(self):
         return "El perro hace: ¡Guau!"
 class Gato(Animal):
+    """Se crea la clase Gato que hereda de Animal"""
     def hacer_sonido(self):
         return "El gato hace: ¡Miau!"
 class Serpiente(Animal):
+    """Se crea la clase Serpiente que hereda de Animal"""
     def hacer_sonido(self):
         return "La serpiente hace: ¡Sss!"
 class Vaca(Animal):
+    """Se crea la clase Vaca que hereda de Animal"""
     def hacer_sonido(self):
         return "La vaca hace: ¡Muu!"
     
