c8ec3118f54cb9486788ba2cd32847ac75c80621
Norma Constanza Giraldogit <conynor@gmail.com>
2026-04-22 14:43:33 -0500

    Se agrega ejercicio 5 y se actualizan los docstrings

--- TALLER_1/ejercicio_5.py
+++ TALLER_1/ejercicio_5.py
@@ -15,29 +15,42 @@ class Vehiculo:
     def detener(self):
         """Se define el método detener que devuelve un mensaje con la velocidad final al detenerse"""
         return f"El vehículo se detiene con una velocidad final de: 0 km/h."
+    
 class Carro(Vehiculo):
     """Se crea la clase hija Carro que hereda de Vehiculo"""
     def __init__(self, velocidad, combustible,aumento,freno, num_puertas):
         super().__init__(velocidad, combustible,aumento,freno)
         self.num_puertas = num_puertas
+    def acelerar(self):
+        """Se redefine el método acelerar para la clase Carro"""
+        return "El carro acelera pisando el acelerador."
 
 class Moto(Vehiculo):
     """Se crea la clase hija Moto que hereda de Vehiculo"""
     def __init__(self, velocidad, combustible,aumento,freno, tipo_moto):
         super().__init__(velocidad, combustible,aumento,freno)
         self.tipo_moto = tipo_moto
+    def acelerar(self):
+        """Se redefine el método acelerar para la clase Moto"""
+        return "La moto acelera generando una rotación en la manija del acelerador."
         
 class Bicicleta(Vehiculo):
     """Se crea la clase hija Bicicleta que hereda de Vehiculo"""
     def __init__(self, velocidad, combustible,aumento,freno, tipo_bici):
         super().__init__(velocidad, combustible,aumento,freno)
         self.tipo_bici = tipo_bici
+    def acelerar(self):
+        """Se redefine el método acelerar para la clase Bicicleta"""
+        return "La bicicleta acelera pedaleando."
         
 class Avion(Vehiculo):
     """Se crea la clase hija Avion que hereda de Vehiculo"""
     def __init__(self, velocidad, combustible,aumento,freno, tipo_avion):
         super().__init__(velocidad, combustible,aumento,freno)
-        self.tipo_avion = tipo_avion   
+        self.tipo_avion = tipo_avion 
+    def acelerar(self):
+        """Se redefine el método acelerar para la clase Avion"""
+        return "El avión acelera generando empuje con los motores."  
         
 carro = Carro(100, "diesel", 20, 30, 4)
 print(f"Carro: \nNúmero de puertas: {carro.num_puertas}")
